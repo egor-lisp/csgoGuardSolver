bf55cee179792da47d93c9ea90cc919295a66af8
egor-lisp <70658544+egor-lisp@users.noreply.github.com>
2023-02-04 15:46:20 +0300

    Update start_steam.py

--- start_steam.py
+++ start_steam.py
@@ -17,19 +17,21 @@ def start_steam_client(username, password, shared_secret):
         f'-login {username} {password} -language english -applaunch 730 -low -nohltv -nosound -novid '
         f'-window -nomouse -w 640 -h 480 +connect {config.server_ip} +exec autoexec.cfg -x 0 -y 0'
     )
-    time.sleep(10)
 
-    # Вводим guard code
-    win_name = 'Steam Sign In'
-    shell = win32com.client.Dispatch("WScript.Shell")
-    shell.SendKeys('%')  # Активируем окно
-    hwnd = win32gui.FindWindowEx(None, None, None, win_name)
-    # print(f'Hwnd: {hwnd}')
-    win32gui.SetForegroundWindow(hwnd)
-    time.sleep(1.5)
-    guard_code = generate_one_time_code(shared_secret)
-    print(f'Guard for account {username}: {guard_code}')
-    for char in guard_code:
-        char_code = utils.VK_CODE[char.lower()]
-        win32api.SendMessage(hwnd, win32con.WM_CHAR, char_code, 0)
-    win32api.SendMessage(hwnd, win32con.WM_KEYDOWN, utils.VK_CODE['enter'], 0)
+    win_name = 'Steam Guard - Computer Authorization Required'
+    success = False
+    while not success:
+        hwnd = win32gui.FindWindowEx(None, None, None, win_name)
+        if hwnd:
+            shell = win32com.client.Dispatch("WScript.Shell")
+            shell.SendKeys('%')  # Активируем окно
+            # print(f'Hwnd: {guard_win_hwnd}')
+            win32gui.SetForegroundWindow(hwnd)
+            time.sleep(1.5)
+            guard_code = generate_one_time_code(shared_secret)
+            print(f'Guard for account {username}: {guard_code}')
+            for char in guard_code:
+                char_code = utils.VK_CODE[char.lower()]
+                win32api.SendMessage(hwnd, win32con.WM_CHAR, char_code, 0)
+            win32api.SendMessage(hwnd, win32con.WM_KEYDOWN, utils.VK_CODE['enter'], 0)
+            success = True
